b75bafad941a483c21bf43045188c7ae56828669
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2023-03-13 06:05:14 -0300

    feat: WIP pattern repository

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,7 +1,6 @@
 import { FastifyReply, FastifyRequest } from "fastify";
-import { prisma } from "@/lib/prisma";
-import { hash } from "bcryptjs";
 import { z } from "zod";
+import { registerUseCase } from "@/use-cases/register";
 
 export async function register(req: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -12,25 +11,12 @@ export async function register(req: FastifyRequest, reply: FastifyReply) {
 
   const { name, email, password } = registerBodySchema.parse(req.body);
 
-  const password_hash = await hash(password, 6);
-
-  const userWithSameEmail = await prisma.user.findUnique({
-    where: {
-      email,
-    },
-  });
-
-  if (userWithSameEmail) {
+  try {
+    await registerUseCase({ name, email, password });
+  } catch (error) {
+    console.log(error);
     return reply.status(409).send();
   }
-
-  await prisma.user.create({
-    data: {
-      name,
-      email,
-      password_hash,
-    },
-  });
 
   return reply.status(201).send();
 }
