dee8bb74a2d1f31612eccc53cb3ef140149ce353
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2023-03-13 05:38:14 -0300

    feat: hash user password

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,5 +1,6 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 import { prisma } from "@/lib/prisma";
+import { hash } from "bcryptjs";
 import { z } from "zod";
 
 export async function register(req: FastifyRequest, reply: FastifyReply) {
@@ -11,11 +12,23 @@ export async function register(req: FastifyRequest, reply: FastifyReply) {
 
   const { name, email, password } = registerBodySchema.parse(req.body);
 
+  const password_hash = await hash(password, 6);
+
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (userWithSameEmail) {
+    return reply.status(409).send();
+  }
+
   await prisma.user.create({
     data: {
       name,
       email,
-      password_hash: password,
+      password_hash,
     },
   });
 
