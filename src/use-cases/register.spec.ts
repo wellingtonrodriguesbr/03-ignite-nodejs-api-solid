39ebb6ee098a010c5f6f40fabc69cf868343571b
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2023-03-18 14:24:47 -0300

    chore: rename variables

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -7,9 +7,9 @@ import { RegisterUseCase } from "./register";
 describe("Register use case", () => {
   it("should be able to register", async () => {
     const usersRepository = new InMemoryUsersRepository();
-    const registerUseCase = new RegisterUseCase(usersRepository);
+    const sut = new RegisterUseCase(usersRepository);
 
-    const { user } = await registerUseCase.execute({
+    const { user } = await sut.execute({
       name: "John Doe",
       email: "johndoe@example.com",
       password: "123456",
@@ -20,9 +20,9 @@ describe("Register use case", () => {
 
   it("should hash user password upon registration", async () => {
     const usersRepository = new InMemoryUsersRepository();
-    const registerUseCase = new RegisterUseCase(usersRepository);
+    const sut = new RegisterUseCase(usersRepository);
 
-    const { user } = await registerUseCase.execute({
+    const { user } = await sut.execute({
       name: "John Doe",
       email: "johndoe@example.com",
       password: "123456",
@@ -38,16 +38,16 @@ describe("Register use case", () => {
 
   it("should not be able to register with same email twice", async () => {
     const usersRepository = new InMemoryUsersRepository();
-    const registerUseCase = new RegisterUseCase(usersRepository);
+    const sut = new RegisterUseCase(usersRepository);
 
-    await registerUseCase.execute({
+    await sut.execute({
       name: "John Doe",
       email: "johndoe@example.com",
       password: "123456",
     });
 
     await expect(() =>
-      registerUseCase.execute({
+      sut.execute({
         name: "John Doe",
         email: "johndoe@example.com",
         password: "123456",
