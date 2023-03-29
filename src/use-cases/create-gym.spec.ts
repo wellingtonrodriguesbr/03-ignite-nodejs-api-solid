a340e488cdc3ca8d1b681a313c3163fee14e4159
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2023-03-29 06:23:41 -0300

    test: test to create gym

---
+++ src/use-cases/create-gym.spec.ts
@@ -0,0 +1,25 @@
+import { InMemoryGymsRepository } from "@/repositories/in-memory/in-memory-gyms-repository";
+import { beforeEach, describe, expect, it } from "vitest";
+import { CreateGymUseCase } from "./create-gym";
+
+let gymsRepository: InMemoryGymsRepository;
+let sut: CreateGymUseCase;
+
+describe("Create gym use case", () => {
+  beforeEach(() => {
+    gymsRepository = new InMemoryGymsRepository();
+    sut = new CreateGymUseCase(gymsRepository);
+  });
+
+  it("should be able to create gym", async () => {
+    const { gym } = await sut.execute({
+      title: "Typescript Gym",
+      description: null,
+      phone: null,
+      latitude: -23.4160128,
+      longitude: -47.7462528,
+    });
+
+    expect(gym.id).toEqual(expect.any(String));
+  });
+});
