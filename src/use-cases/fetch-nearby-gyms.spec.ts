13487b681a7d610d1da66cbed1ad0c8e3818a60c
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2023-04-13 06:03:41 -0300

    test: fetch nearby gyms use case

---
+++ src/use-cases/fetch-nearby-gyms.spec.ts
@@ -0,0 +1,39 @@
+import { describe, it, expect, beforeEach } from "vitest";
+import { InMemoryGymsRepository } from "@/repositories/in-memory/in-memory-gyms-repository";
+import { FetchNearbyGymssUseCaseUseCase } from "./fetch-nearby-gyms";
+
+let gymRepository: InMemoryGymsRepository;
+let sut: FetchNearbyGymssUseCaseUseCase;
+
+describe("Fetch nearby gyms use case", () => {
+  beforeEach(async () => {
+    gymRepository = new InMemoryGymsRepository();
+    sut = new FetchNearbyGymssUseCaseUseCase(gymRepository);
+  });
+
+  it("should be able to fetch nearby gyms", async () => {
+    await gymRepository.create({
+      title: "Near Gym",
+      description: null,
+      phone: null,
+      latitude: -23.4160128,
+      longitude: -47.7462528,
+    });
+
+    await gymRepository.create({
+      title: "Far Gym",
+      description: null,
+      phone: null,
+      latitude: -27.0747279,
+      longitude: -49.4889672,
+    });
+
+    const { gyms } = await sut.execute({
+      userLatitude: -23.4160128,
+      userLongitude: -47.7462528,
+    });
+
+    expect(gyms).toHaveLength(1);
+    expect(gyms).toEqual([expect.objectContaining({ title: "Near Gym" })]);
+  });
+});
