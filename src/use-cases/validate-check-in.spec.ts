631ed1d128afde71fcf618139f0c8ef5c7351202
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2023-04-16 10:01:29 -0300

    test: test validate check-in time

--- src/use-cases/validate-check-in.spec.ts
+++ src/use-cases/validate-check-in.spec.ts
@@ -11,11 +11,11 @@ describe("Validate Check-in use case", () => {
     checkInsRepository = new InMemoryCheckInsRepository();
     sut = new ValidateCheckInUseCase(checkInsRepository);
 
-    /* vi.useFakeTimers(); */
+    vi.useFakeTimers();
   });
 
   afterEach(() => {
-    /* vi.useRealTimers(); */
+    vi.useRealTimers();
   });
 
   it("should be able to validate the check-in", async () => {
@@ -34,5 +34,21 @@ describe("Validate Check-in use case", () => {
     await expect(() =>
       sut.execute({ checkInId: "inexistent-check-in-id" })
     ).rejects.toBeInstanceOf(ResourceNotFoundError);
+  });
+
+  it("should not be able to validate check-in after 20 minutes of its creation", async () => {
+    vi.setSystemTime(new Date(2023, 0, 1, 13, 40));
+
+    const createdCheckIn = await checkInsRepository.create({
+      gym_id: "gym-01",
+      user_id: "user-01",
+    });
+
+    const twentyOneMinutesInMs = 1000 * 60 * 21;
+    vi.advanceTimersByTime(twentyOneMinutesInMs);
+
+    await expect(() =>
+      sut.execute({ checkInId: createdCheckIn.id })
+    ).rejects.toBeInstanceOf(Error);
   });
 });
