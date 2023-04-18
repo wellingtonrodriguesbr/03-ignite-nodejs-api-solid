3889615a6791f70c119a7e2a91487a19556b2693
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2023-04-18 06:25:39 -0300

    feat: returning user data

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,8 +1,16 @@
+import { makeGetUserProfileUseCase } from "@/use-cases/factories/make-get-user-profile-use-case";
 import { FastifyReply, FastifyRequest } from "fastify";
 
 export async function profile(req: FastifyRequest, reply: FastifyReply) {
-  await req.jwtVerify();
+  const getUserProfile = makeGetUserProfileUseCase();
+  const { user } = await getUserProfile.execute({
+    userId: req.user.sub,
+  });
 
-  console.log(req.user.sub);
-  return reply.status(200).send();
+  return reply.status(200).send({
+    user: {
+      ...user,
+      password_hash: undefined,
+    },
+  });
 }
