e6c9053893fe423088698ff0c65cf378923a185c
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2023-09-08 07:22:47 -0300

    test: test e2e register

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,3 +1,22 @@
-import { test } from "vitest";
+import request from "supertest";
+import { app } from "@/app";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
 
-test('ok', async () => {})
+describe("Register (e2e)", () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it("should be able to register", async () => {
+    const response = await request(app.server).post("/users").send({
+      name: "Wellington",
+      email: "wellington2@email.com",
+      password: "123456",
+    });
+
+    expect(response.statusCode).toEqual(201);
+  });
+});
